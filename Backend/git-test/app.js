48d6c01a32ff0e1caea4cc987abb88b8d61ba738
sang woo <tooil1729@gmail.com>
2022-12-28 18:39:16 +0900

    Add: 로그인 기능 구현 완료

--- Backend/git-test/app.js
+++ Backend/git-test/app.js
@@ -32,6 +32,25 @@ feature/signin 브랜치의 경우 app.post('/users/signin', ...)
 feature/signup 브랜치의 경우 app.post('/users/signup', ...)
 */
 
+app.post('users/signin', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await myDataSource.query(
+    ` SELECT
+        users.id
+      FROM
+        users
+      WHERE
+        users.email = ?
+    `, [ email ]
+  );
+
+  if (!user) {
+    res.json({ message : "SIGNUP_REQUIRED"});
+  }
+
+  return res.json({ userId : user.id });
+  
+});
 
 app.listen(PORT, () => {
   myDataSource.initialize()
