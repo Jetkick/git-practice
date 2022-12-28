31a147c519276641136b32e276e4e4d764bca3e1
sang woo <tooil1729@gmail.com>
2022-12-28 18:21:29 +0900

    ADD: 유저 회원 가입 기능 구현 완료

--- Backend/git-test/app.js
+++ Backend/git-test/app.js
@@ -32,6 +32,19 @@ feature/signin 브랜치의 경우 app.post('/users/signin', ...)
 feature/signup 브랜치의 경우 app.post('/users/signup', ...)
 */
 
+app.post('/users/signup', async (req, res) => {
+  const { username, email, password } = req.body
+    return await myDataSource.query(
+       `INSERT INTO
+        users (
+          username,
+          email,
+          password
+        ) VALUES (?, ?, ?)
+       `, [ username, email, password ]
+    );
+    
+});
 
 app.listen(PORT, () => {
   myDataSource.initialize()
